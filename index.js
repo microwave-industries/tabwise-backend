e29fc3fff94334e1fa0e259348fc54b0919d3a4c
Sam Gao <gaoshan_274@hotmail.com>
2019-11-30 21:16:02 +0000

    now + things

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 // packages
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const mm = require('./services/mm');
 const receipt = require('./services/receipt');
@@ -13,9 +14,13 @@ const app = express();
 const port = 8080;
 
 app.use(cookieParser());
+app.use(cors());
 // inject the token into req.query
 app.use((req, res, next) => {
-	if (req.cookies.token != null && req.query.token == null) req.query.token = req.cookies.token;
+	if (req.cookies.token != null && req.query.token == null) {
+		req.query.token = req.cookies.token;
+		req.body.token = req.cookies.token;
+	}
 	next();
 });
 
