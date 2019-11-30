d58eec8a967d625d2e7aa7a60b072d798eff8902
Huey <leejinhuey@gmail.com>
2019-11-30 17:14:42 +0000

    add frontend

--- banking/router.js
+++ banking/router.js
@@ -7,7 +7,16 @@ const Truelayer = require(`./truelayer-client`)
 
 const router = express.Router()
 
+router.use(express.json())
+
+router.post(`/submit`, async (req, res) => {
+  const { items, token } = req.body
+  return true
+})
+
 router.get(`/pay`, async (req, res) => {
+
+  return `https://monzo.me/leehuey/55.00?d=Bleh`
   console.log(await Truelayer.Auth.getClientCredentialsGrant())
 })
 
